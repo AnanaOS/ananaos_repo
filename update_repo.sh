bd22d14024e2e7da1a6df743bd78d255dd1d5514
ToMattBan <souaquelecleyton@yandex.com>
2021-08-04 22:16:41 -0300

    Updated the script do update_repo and finally commiting the right files

--- update_repo.sh
+++ update_repo.sh
@@ -11,7 +11,7 @@ cd x86_64
 rm -rf ananaos_repo*
 
 # Creating new database
-repo-add ananaos.db.tar.gz *.pkg.tar.zst
+repo-add ananaos_repo.db.tar.gz *.pkg.tar.zst
 
 # Asking if the user want to push the changes
 
@@ -22,8 +22,14 @@ read askPush
 if [ "$askPush" == "y" ] || [ "$askPush" == "" ]
     then
 
+    # Comming back one folder
+    cd ..
+
     # Putting the new packages in the repo
     git add *
+
+    #Showing what is different
+    git status
 
     echo "Write here the changes you made"
     read Commit
